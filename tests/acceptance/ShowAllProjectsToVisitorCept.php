24a01ba401540776cd8e3710da4b3cf70d75b7be
Dieze <dieze51@gmail.com>
2014-01-10 17:44:58 +0100

    ShowAllProjectsToVisitorCept fix typo, documentation

--- tests/acceptance/ShowAllProjectsToVisitorCept.php
+++ tests/acceptance/ShowAllProjectsToVisitorCept.php
@@ -1,15 +1,17 @@
 <?php
+
 /**
  * ShowAllProjectsToVisitor acceptance test
- * @author     Adrien Desfourneaux
+ * @author     Adrien Desfourneaux (aka Dieze) <dieze51@gmail.com>
  * @package    DzProject
  * @subpackage Acceptance
  * @category   Test
  * @license    http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2
  */
+
 $I = new WebGuy($scenario);
 $I->wantTo('See all projects');
-$I->amOnPage('/projects/show-all/all');
+$I->amOnPage('/project/show-all/all');
 
 $I->see('Project');
 $I->see('Description');
