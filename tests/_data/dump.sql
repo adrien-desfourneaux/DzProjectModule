8c42b9b40b7daeb2919d485450ebf88be33fb70d
Dieze <dieze51@gmail.com>
2014-01-10 21:54:56 +0100

    Configure codeception Db module for acceptance tests

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,1 +1,9 @@
 /* Replace this file with actual dump of your database */
+
+CREATE TABLE project
+(
+    project_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    display_name VARCHAR(50) NOT NULL UNIQUE,
+    begin_date INTEGER NOT NULL,
+    end_date INTEGER NOT NULL
+);
