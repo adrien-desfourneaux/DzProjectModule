ea11484c5c7d0f2bb970e5cac5a5e5bc585dd174
Dieze <dieze51@gmail.com>
2014-01-15 00:35:26 +0100

    init_autoloader docblock

--- init_autoloader.php
+++ init_autoloader.php
@@ -1,3 +1,11 @@
-< ?php
+<?php
+
+/**
+ * Autoload file for the ZF2 Codeception module
+ * @author     Adrien Desfourneaux (aka Dieze) <dieze51@gmail.com>
+ * @package    DzProject
+ * @category   Autoload
+ * @license    http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2
+ */
 
 require_once __DIR__ . '/../../vendor/autoload.php';
