e55d1d0c088d95591d543ba76c4bfc093018e065
Dieze <dieze51@gmail.com>
2014-01-28 00:40:19 +0100

    application.config.php not only related to testing but also to development

--- config/application.config.php
+++ config/application.config.php
@@ -1,6 +1,12 @@
 <?php
 /**
  * Test application configuration file.
+ * 
+ * This file contains only the modules really needed for
+ * the DzProject module to work.
+ *
+ * This file should be run either by /public/dz-project.php
+ * or /public/test/dz-project.php
  *
  * PHP version 5.3.3
  *
@@ -8,7 +14,7 @@
  * @package  DzProject
  * @author   Adrien Desfourneaux (aka Dieze) <dieze51@gmail.com>
  * @license  http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2
- * @link     https://github.com/dieze/DzProject/blob/master/tests/application.config.php
+ * @link     https://github.com/dieze/DzProject/blob/master/config/application.config.php
  */
 return array(
     'modules' => array(
