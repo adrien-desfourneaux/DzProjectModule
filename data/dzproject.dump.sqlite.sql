c44502896c262396e78f79baf2a4e37caa1bb5e3
Dieze <dieze51@gmail.com>
2014-02-07 12:03:49 +0100

    Use strftime() sqlite function in dump file

--- data/dzproject.dump.sqlite.sql
+++ data/dzproject.dump.sqlite.sql
@@ -1,10 +1,71 @@
-/*
-Timestamps
-01/01/2013	1356994800 
-01/01/2014	1388530800 
-01/01/2015	1420066800 
-*/
+INSERT INTO project (
+    project_id,
+    display_name,
+    begin_date,
+    end_date
+) VALUES (
+    1,
+    'Projet non débuté',
+    strftime('%s', 'now', 'start of day', '-1 hour', '+6 months'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '+1 year')
+);
 
-INSERT INTO project (project_id, display_name, begin_date, end_date) VALUES (1, 'Projet terminé', 1356994800, 1388530800);
-INSERT INTO project (project_id, display_name, begin_date, end_date) VALUES (2, 'Projet actif 1', 1356994800, 1420066800);
-INSERT INTO project (project_id, display_name, begin_date, end_date) VALUES (3, 'Projet actif 2', 1388530800, 1420066800);
+INSERT INTO project (
+    project_id,
+    display_name,
+    begin_date,
+    end_date
+) VALUES (
+    2,
+    "Projet qui débute aujourd'hui",
+    strftime('%s', 'now', 'start of day', '-1 hour'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '+2 years')
+);
+
+INSERT INTO project (
+    project_id,
+    display_name,
+    begin_date,
+    end_date
+) VALUES (
+    3,
+    'Projet actif 1',
+    strftime('%s', 'now', 'start of day', '-1 hour', '-1 day'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '+3 years')
+);
+
+INSERT INTO project (
+    project_id,
+    display_name,
+    begin_date,
+    end_date
+) VALUES (
+    4,
+    'Projet actif 2',
+    strftime('%s', 'now', 'start of day', '-1 hour', '-1 day'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '+3 years')
+);
+
+INSERT INTO project (
+    project_id,
+    display_name,
+    begin_date,
+    end_date
+) VALUES (
+    5,
+    "Projet qui se termine aujourd'hui",
+    strftime('%s', 'now', 'start of day', '-1 hour', '-1 day'),
+    strftime('%s', 'now', 'start of day', '-1 hour')
+);
+
+INSERT INTO project (
+    project_id,
+    display_name,
+    begin_date,
+    end_date
+) VALUES (
+    6,
+    'Projet terminé',
+    strftime('%s', 'now', 'start of day', '-1 hour', '-2 days'),
+    strftime('%s', 'now', 'start of day', '-1 hour', '-1 day')
+);
