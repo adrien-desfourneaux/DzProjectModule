b57b20181f6c7cf7bae1d6b2c1b019e55d9b7db5
Dieze <dieze51@gmail.com>
2014-02-12 16:06:06 +0100

    application files to move to public dir

---
+++ dzproject.test.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Lance l'application de test du module DzProject.
+ * Seuls le module DzProject et ses dépendances seront chargées.
+ * Cela permet le test du module DzProject seul, séparé
+ * du reste de l'application.
+ *
+ * Copier ce fichier dans le répertoire public de votre application
+ *
+ * Utilisation :
+ * http://mydomain/dzproject.test.php/project[/:action]
+ *
+ * PHP Version 5.3.3
+ *
+ * @category Source
+ * @package  DzProject
+ * @author   Adrien Desfourneaux (aka Dieze) <dieze51@gmail.com>
+ * @license  http://opensource.org/licenses/GPL-2.0 GNU General Public License, version 2
+ * @link     https://github.com/dieze/DzProject/blob/master/dzproject.test.php
+ */
+
+/**
+ * Définit l'environnement d'exécution pour le module.
+ */
+define('DZPROJECT_ENV', 'test');
+
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(require 'module/DzProject/config/application.config.php')->run();
